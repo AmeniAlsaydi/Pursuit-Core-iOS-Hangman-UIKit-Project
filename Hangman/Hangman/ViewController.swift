b71f0a943cf02d4d46faceb0d141692fc79b0924
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-13 22:46:47 -0500

    layout complete

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var wordEntry: UITextField!
+    var enteredWord = ""
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    wordEntry.delegate = self
   }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let word = textField.text else { return false }
+        enteredWord = word
+        print(word)
+        return true
+    }
 
 
 }
